aacae64ecd7a2a8d99541cb373ebedf5459455e3
viktor_strelianyi <chmpain@gmail.com>
2025-06-06 15:53:14 +0300

    - updated db

--- src/prisma/prisma.module.ts
+++ src/prisma/prisma.module.ts
@@ -1,1 +1,8 @@
 import { Global, Module } from '@nestjs/common';
+
+@Module({
+  imports: [PrismaModule],
+  providers: [PrismaService],
+  exports: [PrismaService],
+})
+export class PrismaModule {}
